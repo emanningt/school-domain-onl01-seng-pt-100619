24bbfc18f20cfe1339c00d698a4e7d55132886b1
Eric Manning <emanningmusic@yahoo.com>
2019-11-14 05:07:16 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,13 @@
-# code here!
+class School
+  
+  attr_accessor :school, :add_student, :grade
+  
+  roster = {}
+  
+  def initialize(school)
+    @school = school
+  end
+  
+end 
+
+school = School.new("Bayside High School")
